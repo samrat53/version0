cdd28553f9f6653e47a97870041d83733cda3f8a
samrat53 <samrat05hccs@gmail.com>
2024-03-09 02:17:17 +0530

    build projects section

---
+++ src/components/NextButton.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const NextButton = () => {
+
+  return (
+    <button className="nextbutton">
+      <div class="svg-wrapper-1">
+        <div class="svg-wrapper">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="12"
+            height="12"
+          >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path
+              fill="currentColor"
+              d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </button>
+  );
+};
+
+export default NextButton;
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,18 +1,75 @@
 import React from "react";
+import NextButton from "../components/NextButton";
 
 const Projects = () => {
+  const data = [
+    {
+      name: "Personalised Chatbot",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus odio sed similique fugiat harum nostrum tenetur, illo doloribus ad maiores consequatur adipisci placeat perspiciatis expedita atque architecto praesentium! Praesentium, omnis?",
+      techstacks: ["Google gemini", "Express.js", "React.js"],
+      link: "https://github.com",
+    },
+    {
+      name: "Personalised Chatbot",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus odio sed similique fugiat harum nostrum tenetur, illo doloribus ad maiores consequatur adipisci placeat perspiciatis expedita atque architecto praesentium! Praesentium, omnis?",
+      techstacks: ["Google gemini", "Express.js", "React.js"],
+      link: "https://github.com",
+    },
+    {
+      name: "Personalised Chatbot",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus odio sed similique fugiat harum nostrum tenetur, illo doloribus ad maiores consequatur adipisci placeat perspiciatis expedita atque architecto praesentium! Praesentium, omnis?",
+      techstacks: ["Google gemini", "Express.js", "React.js"],
+      link: "https://github.com",
+    },
+  ];
+
   return (
     <div>
-      <div className="card1">
-        <div className="content1">
-          <p className="heading1">Card Hover Effect</p>
-          <p className="para1">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
-            laboriosam at voluptas minus culpa deserunt delectus sapiente
-            inventore pariatur
-          </p>
-          <button className="btn1">Read more</button>
-        </div>
+      <div className="text-center underline font-bold text-2xl my-9">
+        Projects & Skills
+      </div>
+      <div className="project-container lg:flex md:flex sm:flex md:justify-between lg:justify-between space-x-5 lg:px-[11rem] my-4">
+        {data.map((project, index) => {
+          return (
+            <div className="card1" key={index}>
+              <div className="content1">
+                <p className="heading1 w-full">{project.name}</p>
+                <p className="para1">{project.description}</p>
+                <div className="flex space-x-1 my-8">
+                  {project.techstacks.map((item, index) => {
+                    return (
+                      <div key={index} className="tech-stacks text-sm">
+                        {item}
+                      </div>
+                    );
+                  })}
+                </div>
+                <a href={project.link}>
+                  <button className="absolute bottom-0 right-0 mt-6 -mb- ml-2 w-full nextbutton Btn">
+                    Check it out
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16rem"
+                      height="16rem"
+                      fill="currentColor"
+                      class="bi bi-arrow-right-circle"
+                      viewBox="0 0 16 16"
+                      className="svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
+                      />
+                    </svg>
+                  </button>
+                </a>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
